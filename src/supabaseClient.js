9ecd0239ef802866207eaf40f3b3370526a344d9
atharva <baodhankaratharva@gmail.com>
2025-07-02 09:12:13 +0530

    added ENV

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://fgzwodftkglvngkmkwnv.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZnendvZGZ0a2dsdm5na21rd252Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEzNzM4OTAsImV4cCI6MjA2Njk0OTg5MH0.9eEt44ULmRjgyUL3_SaL6yi0Rhsab2qWEpIxJU1GNmw';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey); 
