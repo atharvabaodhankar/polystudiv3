e171bf536cb3264153bc36f80a39aa359a8f8c16
AtharvaPC <baodhankaratharva@gmail.com>
2025-07-02 07:41:41 +0530

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,9 @@ import Signup from './pages/Signup';
 
 const App = () => (
   <Router>
-    <div className="flex flex-col min-h-screen">
+    <div className="min-h-screen flex flex-col bg-white">
       <Navbar />
-      <main className="flex-1">
+      <main className="flex-1 pt-20">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />} />
